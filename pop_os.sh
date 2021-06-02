b9001af102d7f162a24e8985c3c7b592bb8294ac
wjuniorbh92 <68206230+wjuniorbh92@users.noreply.github.com>
2021-06-02 10:26:01 -0300

    Novos Apps

--- pop_os.sh
+++ pop_os.sh
@@ -2,38 +2,46 @@
 
 sudo rm /var/lib/dpkg/lock-frontend; sudo rm /var/cache/apt/archives/lock ;
 
-sudo dpkg --add-architecture i386 
+sudo dpkg --add-architecture i386 -y
 
 timedatectl set-local-rtc 1 --adjust-system-clock
 
+sudo apt install snapd -y
 # Installs all required software
-sudo apt install flatpak
-flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo apt install flatpak -y
+
 # Update packages
-sudo apt update
+sudo apt update -y
 sudo apt upgrade -y
 flatpak update -y
 #######  Instalando VS Code
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt install apt-transport-https
-sudo apt update
-sudo apt install code 
+sudo apt install apt-transport-https -y
+sudo apt update -y
+sudo apt install code  -y
 
+sudo apt-get install libmysqlclient-dev -y
 
 # Install APT packages
-sudo apt install -y \
-gnome-tweaks\
-ubuntu-restricted-extras \
-variety vlc \
-preload \ 
-stacer \ vlc \
+sudo apt install gnome-tweaks -y 
+sudo apt install vlc -y 
+sudo apt install ubuntu-restricted-extras -y 
+sudo apt install stacer -y 
 
 # Install Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
 rm google-chrome-stable_current_amd64.deb
+
+#Install Github
+wget https://github.com/shiftkey/desktop/releases/download/release-2.6.3-linux1/GitHubDesktop-linux-2.6.3-linux1.deb
+ 
+sudo apt-get install gdebi-core
+sudo gdebi ./GitHubDesktop-linux-2.6.3-linux1.deb -y
+rm GitHubDesktop-linux-2.6.3-linux1.deb
+
 
 # Install MiniConda
 mkdir -p ~/miniconda3
@@ -42,19 +50,79 @@ bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init bash
 ~/miniconda3/bin/conda init zsh
+sudo apt install virtualenv -y
 
 # Configure Anaconda 
 conda config --set changeps1 True
 
+#Install Team Viewer
+
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+sudo apt install ./teamviewer_amd64.deb -y
+rm teamviewer_amd64.deb
+
 #Instal Oh My Bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 
-# Install Flatpaks
-flatpak install -y \
-io.dbeaver.DBeaverCommunity	\
-nl.hjdskes.gcolor3 \
-org.flameshot.Flameshot\
-com.github.phase1geo.minder \ 
+#OBS
+sudo apt-get install ffmpeg -y
+sudo add-apt-repository ppa:obsproject/obs-studio
+sudo apt-get update && sudo apt-get install obs-studio -y
+
+
+## Notion
+
+wget https://notion.davidbailey.codes/notion-linux.list
+sudo mv notion-linux.list /etc/apt/sources.list.d/notion-linux.list
+sudo apt update && sudo apt install notion-desktop
 
 sudo apt-get install libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386 -y
+
+
+# Sync
+sudo apt-get install apt-transport-https
+echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install syncthing -y
+
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+
+systemctl --user enable syncthing.service
+
+#Docker
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install docker-ce -y
+sudo usermod -aG docker wjunior
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+##PG Admin
+
+sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+sudo apt install pgadmin4-desktop -y
+
+#discord
+flatpak install flathub com.discordapp.Discord 
+
+
+sudo apt update
+sudo apt upgrade  - y --allow-downgrades
+sudo apt autoclean
+sudo apt autoremove -y
+
+sudo apt install --reinstall bluez gnome-bluetooth indicator-bluetooth pulseaudio-module-bluetooth -y
+sudo apt install blueman -y
+
+
+sudo apt install remmina remmina-plugin-rdp remmina-plugin-secret -y
+
+
 
